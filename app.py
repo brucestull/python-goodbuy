c9db027af3bff6ec31c4f795484a9b6523b72f20
Bruce Stull <bruce.stull@gmail.com>
2025-07-11 19:02:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def goodbye_world():
     return "<h1>Goodbuy, World! Enjoy the Sails and Bar Guns!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
